e32f65bd7fe4f3a7c7736895f6e46811ac9aa291
Arthur Leonard Andersen <aandersen@velalu.qa>
2018-09-13 17:01:48 +0200

    Change emacs.d page title to `Emacs.d`

--- prepare_files.rb
+++ prepare_files.rb
@@ -1,5 +1,5 @@
 emacs_d = File.read('emacs.d.org')
 
 if emacs_d !~ /\A---/
-  File.write('emacs.d.org', "---\ntitle: My emacs.d\nlayout: page\n---\n#{emacs_d}")
+  File.write('emacs.d.org', "---\ntitle: Emacs.d\nlayout: page\n---\n#{emacs_d}")
 end
